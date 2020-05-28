5f0489a153da349a06c58481548e039917e6ba6a
Lluvia Frias <lluvi.frias@gmail.com>
2020-05-28 17:15:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,11 +8,19 @@
 #   return reversed_string.join(" ")
 # end
 
-
 def reverse_each_word(string_argument)
   reversed_string = string_argument.split()
-  reversed_string.collect do |word|
-    word.reversed!
-  end
-return reversed_string.join(" ")
+  reversed_string.collect do|word|
+    word.reverse!
+    end
+
+  return reversed_string.join(" ")
 end
+
+# def reverse_each_word(string_argument)
+#   reversed_string = string_argument.split()
+#   reversed_string.collect do |word|
+#     word.reversed!
+#   end
+# return reversed_string.join(" ")
+# end
