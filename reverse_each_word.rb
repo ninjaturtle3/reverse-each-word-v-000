fb85e27e1b19c47d98d5e24cd36e8e234ffe28c2
Lluvia Frias <lluvi.frias@gmail.com>
2020-05-28 17:44:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,13 +10,19 @@
 # end
 
 
-#using #.collect
-string_argument = "Hello there, and how are you?"
+# #using #.collect
+# string_argument = "Hello there, and how are you?"
+# def reverse_each_word(string_argument)
+#   reversed_string = string_argument.split()
+#   reversed_string.collect do |word|
+#     word.reverse!
+#   end
+#
+# puts reversed_string.join(" ")
+# end
+
 def reverse_each_word(string_argument)
-  reversed_string = string_argument.split()
-  reversed_string.collect do |word|
+  string_argument.split.collect do |word|
     word.reverse!
   end
-
-puts reversed_string.join(" ")
-end
+  .join(" ")
