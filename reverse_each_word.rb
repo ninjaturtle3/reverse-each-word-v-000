bc77d5ae2bf181972663bb2ed509dac0bf652e2c
Lluvia Frias <lluvi.frias@gmail.com>
2020-05-28 17:08:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,4 @@
-# def reverse_each_word (string_argument)
-#   string_argument.split() do|words|
-#     words.each.reverse!.join(" ")
-#   puts "#{words}"
-# end
-#
-#   end
+
 def reverse_each_word (string_argument)
   reversed_string = string_argument.split()
   reversed_string.each do|word|
